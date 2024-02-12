deec6278f06aecb7980320a49841be01b9f0e401
MateusDias <testdreder@outlook.com>
2024-02-11 22:42:12 -0300

    projeto attr

--- attrs/attrs.py
+++ attrs/attrs.py
@@ -1,9 +1,8 @@
 string = "oi tudo bem"
-metode = "upper"
+metode = "upper" #Pemite alterar o metodo para upper, lo , werstrip etc...
     
 if hasattr(string,metode):
     print(getattr(string,metode)())
     print(f"É um metodo {string} é um {metode}")
 else:
     print(f"Não é um metodo{metode}")
-
